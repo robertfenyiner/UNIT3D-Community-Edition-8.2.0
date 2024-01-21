ac5ed28c8f0413e0fb047743520460a0170cff53
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2024-01-20 21:36:28 -0500

    Update HomeController.php

--- app/Http/Controllers/Staff/HomeController.php
+++ app/Http/Controllers/Staff/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
             //$certificate = $request->secure() ? SslCertificate::createForHostName(config('app.url')) : '';
         //} catch (Exception) {
             $certificate = '';
-        }
+       // }
 
         // System Information
         $systemInformation = new SystemInformation();
