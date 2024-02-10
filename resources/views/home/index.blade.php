9fdd38802ac7cf62a09af689a53e67411927af4f
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2024-02-10 16:10:33 -0500

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -17,6 +17,6 @@
     @include('blocks.top_torrents')
     @include('blocks.top_uploaders')
     @include('blocks.latest_topics')
-    @include('blocks.latest_posts')
+    <!--@include('blocks.latest_posts')-->
     @include('blocks.online')
 @endsection
