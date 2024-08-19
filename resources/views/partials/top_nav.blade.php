cde3382c8a09ba21fbd61be800adf4a73ae47360
gosunuel <manueldiazisaza@gmail.com>
2024-08-18 19:18:22 -0500

    Ajustando Logo

--- resources/views/partials/top_nav.blade.php
+++ resources/views/partials/top_nav.blade.php
@@ -4,7 +4,7 @@
             <!--<i class="fal fa-tv-retro"></i>-->
             <!--<span class="top-nav__site-logo">{{ \config('other.title') }}</span>-->
 
-            <a href="{{ route('home.index') }}"><img src="{{ url('/img/ltsmall.png') }}">
+            <a href="{{ route('home.index') }}" style="height: 32px;"><img src="{{ url('/img/ltsmall.png') }}">
         </a>
         <livewire:quick-search-dropdown />
     </div>
